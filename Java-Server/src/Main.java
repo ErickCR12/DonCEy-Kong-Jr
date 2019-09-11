d6ac2081e2ab2d77872ef21509ccef1324d2e167
Jose Acuña <jodaniel1409@gmail.com>
2019-09-10 18:33:15 -0600

    Created structure for the GUI
    Change Java language level to 8, so it can work with JavaFX

---
+++ Java-Server/src/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("ui/resources/main_menu.fxml"));
+        primaryStage.setTitle("Donkey Kong Jr");
+        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
