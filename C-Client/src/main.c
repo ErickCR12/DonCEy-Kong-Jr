e9835372d028369f88cf5b7cc29a13a916169d34
Erick B <Erick.cr12@outlook.com>
2019-09-19 00:04:59 -0600

    Basic window example in Allegro

--- C-Client/src/main.c
+++ C-Client/src/main.c
@@ -2,26 +2,42 @@
 // Created by José Acuña on 17/09/2019.
 //
 
-#include <stdio.h>
 #include <allegro5/allegro.h>
-#include <allegro5/allegro_native_dialog.h>
-#include <unistd.h>
 
-int main (int argc, char **argv){
-    ALLEGRO_DISPLAY *display = NULL;
 
-    if (!al_init()){
-        al_show_native_message_box(NULL, NULL, NULL, "failed to init allegro!", NULL, 0);
-        return -1;
+int main()
+{
+    ALLEGRO_DISPLAY * display;
+    ALLEGRO_EVENT_QUEUE *queue;
+    ALLEGRO_TIMER *timer;
+
+    al_init();
+    display = al_create_display(640, 480);
+    queue = al_create_event_queue();
+    timer = al_create_timer(1.0 / 60.0);
+
+    al_install_keyboard();
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_display_event_source(display));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    bool running = true;
+    while (running) {
+
+
+        ALLEGRO_EVENT event;
+        al_wait_for_event(queue, &event);
+        if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+            running = false;
+
+        if (event.type == ALLEGRO_EVENT_TIMER) {
+            al_flip_display();
+        }
     }
-    display = al_create_display(600, 600);
-    if (!display){
-        al_show_native_message_box(NULL, NULL, NULL, "failed to init display!", NULL, 0);
-        return -1;
-    }
-
-    sleep(5);
 
     al_destroy_display(display);
+    al_uninstall_keyboard();
+    al_destroy_timer(timer);
+
     return 0;
 }
